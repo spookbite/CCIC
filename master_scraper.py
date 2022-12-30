1ccaba05c92db6e63f33a49bdb66fae0dc63954d
Palkit Lohia <palkitlohia2001@outlook.com>
2022-12-30 23:58:10 +0530

    testing actions

--- master_scraper.py
+++ master_scraper.py
@@ -3,15 +3,15 @@ import index_scraper as ids
 
 
 def main():
-    ns.moneycontrol()
-    ns.grow()
-    ns.upstox_fo()
-    ns.upstox_mo()
-    ns.capital()
-    ids.eqsis()
-    ids.mc_cash()
-    ids.mc_fno()
-    ids.nse_india()
+    # ns.moneycontrol()
+    # ns.grow()
+    # ns.upstox_fo()
+    # ns.upstox_mo()
+    # ns.capital()
+    # ids.eqsis()
+    # ids.mc_cash()
+    # ids.mc_fno()
+    # ids.nse_india()
     ids.investing()
 
 if __name__ == "__main__":
